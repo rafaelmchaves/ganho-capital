26820e7d36f323109e7de4d7b9d2fea930193708
Rafael Marques Chaves <rafaelmchaves@gmail.com>
2024-03-03 10:14:41 -0300

    Create cli command

--- src/main/java/br/com/nubank/input/InputProcessor.java
+++ src/main/java/br/com/nubank/input/InputProcessor.java
@@ -4,30 +4,38 @@ import br.com.nubank.model.Operation;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class InputProcessor {
 
-    public List<Operation[]> getInputData() {
-        final List<String> lines = readInputLines();
-        return getOperationsFromJson(lines);
+    public List<Operation[]> getInputData() throws IOException {
+        System.out.println("passou2");
+        return readInputLines();
     }
 
-    private static List<String> readInputLines() {
-        final Scanner scanner = new Scanner(System.in);
+    private static List<Operation[]> readInputLines() throws IOException {
         List<String> lines = new ArrayList<>();
-        while (scanner.hasNextLine()) {
-            final var text = scanner.nextLine();
-            if (text.isEmpty()) {
-                break;
-            }
-            lines.add(text);
-        }
+        var test = new String(System.in.readAllBytes());
+        System.out.println("passou3: " + test);
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<Operation[]> operations = new ArrayList<>();
+        operations.add(objectMapper.readValue(test, Operation[].class));
 
-        scanner.close();
-        return lines;
+        return operations;
+//        while (scanner.hasNextLine()) {
+//            final var text = scanner.nextLine();
+//            if (text.isEmpty()) {
+//                break;
+//            }
+//            lines.add(text);
+//        }
+//
+//        scanner.close();
     }
 
     private static List<Operation[]> getOperationsFromJson(List<String> lines) {
